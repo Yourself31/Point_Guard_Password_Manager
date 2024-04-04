6cfb8edc5f02fbf837ab3cd1c375805c5094ea1c
Isaiah Velez <isaiahmvelez@gmail.com>
2024-04-04 13:36:18 -0400

    Save; creating password model still

--- PointGuard/content/models.py
+++ PointGuard/content/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class PasswordCategory(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='password_categories')
+
+    def __str__(self):
+        return self.name
+
+class UserPassword(models.Model):
+    category = models.ForeignKey(PasswordCategory, on_delete=models.SET_NULL, null=True, blank=True, related_name='passwords')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_passwords')
+    website = models.CharField(max_length=255)
+    username = models.CharField(max_length=255, blank=True)
+    # WARNING: Storing passwords in plaintext is highly insecure.
+    # This field should instead store an encrypted password and be decrypted only when necessary.
+    password = models.CharField(max_length=255)
+    notes = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.website} - {self.username}"
