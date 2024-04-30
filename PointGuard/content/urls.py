48f4cc9056e6fb92e970723c81b5bb83dc9a2fb0
Isaiah Velez <isaiahmvelez@gmail.com>
2024-04-30 08:03:41 -0400

    Sync

--- PointGuard/content/urls.py
+++ PointGuard/content/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("add_password/", views.add_password, name='add_password'),
+    path('add-category/', views.add_password_category, name='add_password_category')
 ]
--- PointGuard/content/views.py
+++ PointGuard/content/views.py
@@ -1,13 +1,34 @@
 from django.shortcuts import render, redirect
 from .models import UserPassword, PasswordCategory  # Adjusted import to match the application's context
-from .forms import UserPasswordForm
+from .forms import UserPasswordForm, PasswordCategoryForm
 
 # Create your views here.
+# def home(request):
+#     # Retrieve all password categories sorted by name
+#     password_categories = PasswordCategory.objects.all().order_by('name')
+#     # Render the page with the information about the password categories
+#     return render(request, 'content/home.html', {'password_categories': password_categories})
+
 def home(request):
-    # Retrieve all password categories sorted by name
-    password_categories = PasswordCategory.objects.all().order_by('name')
-    # Render the page with the information about the password categories
-    return render(request, 'content/home.html', {'password_categories': password_categories})
+    if request.user.is_authenticated:
+        user_passwords = UserPassword.objects.filter(user=request.user).order_by('-created_at')
+        return render(request, 'content/home.html', {'user_passwords': user_passwords})
+    else:
+        return render(request, 'content/home.html', {'message': 'Please log in to view passwords.'})
+    
+    
+def add_password_category(request):
+    if request.method == 'POST':
+        form = PasswordCategoryForm(request.POST)
+        if form.is_valid():
+            new_category = form.save(commit=False)
+            new_category.user = request.user  # Assuming categories are user-specific
+            new_category.save()
+            return redirect('content/home.html')  # Redirect to a relevant page
+    else:
+        form = PasswordCategoryForm()
+
+    return render(request, 'content/add_password_category.html', {'form': form})
 
 def add_password(request):
     # Upon submission, retrieve and save the form data if it is valid. Then redirect to the home page.
